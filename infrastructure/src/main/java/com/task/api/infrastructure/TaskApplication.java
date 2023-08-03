581066312bdae763280c54d248237d4a8392fc0f
Sesaque Cruz <cruz.sesaque@gmail.com>
2023-08-03 17:39:33 -0300

    feat: add api documentation

--- infrastructure/src/main/java/com/task/api/infrastructure/TaskApplication.java
+++ infrastructure/src/main/java/com/task/api/infrastructure/TaskApplication.java
@@ -1,10 +1,25 @@
 package com.task.api.infrastructure;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "Task Manager", description = "A Task Manager Service with Java and Spring")
+)
+@SecurityScheme(
+        name = "Bearer",
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
 public class TaskApplication {
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
--- infrastructure/src/main/java/com/task/api/infrastructure/api/TaskApi.java
+++ infrastructure/src/main/java/com/task/api/infrastructure/api/TaskApi.java
@@ -5,6 +5,12 @@ import com.task.api.infrastructure.task.models.CreateTaskRequest;
 import com.task.api.infrastructure.task.models.DeleteTaskRequest;
 import com.task.api.infrastructure.task.models.TaskResponse;
 import com.task.api.infrastructure.task.models.UpdateTaskRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -16,19 +22,50 @@ public interface TaskApi {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    ResponseEntity<Void> createTask(JwtAuthenticationToken auth, @RequestBody CreateTaskRequest body);
+    @Operation(
+            summary = "Create a task",
+            security = @SecurityRequirement(name = "Bearer")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Task created"),
+            @ApiResponse(responseCode = "401", description = "User not authenticated", content = @Content),
+            @ApiResponse(responseCode = "422", description = "Invalid data", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error", content = @Content)
+    })
+    ResponseEntity<Void> createTask(@Schema(hidden = true) JwtAuthenticationToken auth, @RequestBody CreateTaskRequest body);
 
     @GetMapping(
             value = "{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    ResponseEntity<TaskResponse> findTask(JwtAuthenticationToken auth, @PathVariable(name = "id") String id);
+    @Operation(
+            summary = "Find a task",
+            security = @SecurityRequirement(name = "Bearer")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task found"),
+            @ApiResponse(responseCode = "401", description = "User not authenticated", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid param", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error", content = @Content)
+    })
+    ResponseEntity<TaskResponse> findTask(@Schema(hidden = true) JwtAuthenticationToken auth, @PathVariable(name = "id") String id);
 
     @GetMapping(
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(
+            summary = "List tasks",
+            security = @SecurityRequirement(name = "Bearer")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tasks found"),
+            @ApiResponse(responseCode = "401", description = "User not authenticated", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid param", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error", content = @Content)
+    })
     ResponseEntity<Page<TaskResponse>> listTask(
-            JwtAuthenticationToken auth,
+            @Schema(hidden = true) JwtAuthenticationToken auth,
             @RequestParam(name = "page", required = false, defaultValue = "0") int page,
             @RequestParam(name = "size", required = false, defaultValue = "10") int size,
             @RequestParam(name = "term", required = false, defaultValue = "") String term,
@@ -40,11 +77,33 @@ public interface TaskApi {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    ResponseEntity<Void> updateTask(JwtAuthenticationToken auth, @RequestBody UpdateTaskRequest body);
+    @Operation(
+            summary = "Update a task",
+            security = @SecurityRequirement(name = "Bearer")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Task updated"),
+            @ApiResponse(responseCode = "401", description = "User not authenticated", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = @Content),
+            @ApiResponse(responseCode = "422", description = "Invalid data", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error", content = @Content)
+    })
+    ResponseEntity<Void> updateTask(@Schema(hidden = true) JwtAuthenticationToken auth, @RequestBody UpdateTaskRequest body);
 
     @DeleteMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    ResponseEntity<Void> deleteTask(JwtAuthenticationToken auth, @RequestBody DeleteTaskRequest body);
+    @Operation(
+            summary = "Delete a task",
+            security = @SecurityRequirement(name = "Bearer")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Task deleted"),
+            @ApiResponse(responseCode = "401", description = "User not authenticated", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = @Content),
+            @ApiResponse(responseCode = "422", description = "Invalid data", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error", content = @Content)
+    })
+    ResponseEntity<Void> deleteTask(@Schema(hidden = true) JwtAuthenticationToken auth, @RequestBody DeleteTaskRequest body);
 }
